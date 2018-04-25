cf7ed155792c62aee8264a28b1ffd01134948b27
HongQirong <ke80188@gmail.com>
2018-04-25 09:56:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView view = (TextView) findViewById(R.id.String1);
+
+        Bundle bundle = getIntent().getExtras();
+        String msg = bundle.getString("message");
+        view.setText("B10401063："+ msg);
+    }
+}
